d0e83b8a574ba55802c6c02c25775f6bb11bb00d
euclidespaim <euclidespaim@gmail.com>
2020-11-23 17:55:29 -0300

    Updates

---
+++ precache-manifest.925128d7a6474add8b579db24d7391a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba52df0a0ada6622bd0a83364f310264",
+    "url": "/movie-db/index.html"
+  },
+  {
+    "revision": "34ad7d59d22d4b520ff0",
+    "url": "/movie-db/static/css/main.e67b5c5f.chunk.css"
+  },
+  {
+    "revision": "5b4a7698c95a23887146",
+    "url": "/movie-db/static/js/2.632e9e22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-db/static/js/2.632e9e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34ad7d59d22d4b520ff0",
+    "url": "/movie-db/static/js/main.434e7a75.chunk.js"
+  },
+  {
+    "revision": "1e35b73533c8089232bd",
+    "url": "/movie-db/static/js/runtime-main.58960b7a.js"
+  }
+]);
